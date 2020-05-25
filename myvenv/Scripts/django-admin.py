9fbf35588e89e3e4baa1bdf12f7712814d938230
aljaba1970 <alejandrobartolome@gmail.com>
2020-05-25 18:31:14 -0300

    Mi aplicación Django Girls, primer commit

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\my-first-blog-master\my-first-blog-master\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alejandro/Documentos/Python/Django/djangogirls/myvenv"
+set -gx VIRTUAL_ENV "/home/alejandro/my-first-blog-master/my-first-blog/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
